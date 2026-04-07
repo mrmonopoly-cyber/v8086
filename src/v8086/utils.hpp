3eb4d4266b0b88028e1c52e1334cb80482dba592
Alberto Damo <alberto.damo@gmail.com>
2026-04-07 15:06:18 +0200

    fix(log): added new line to log

--- src/v8086/utils.hpp
+++ src/v8086/utils.hpp
@@ -1,5 +1,5 @@
 #pragma once
 
-#define TODO(msg) do{printf("%s.%d: " msg, __FILE__, __LINE__); exit(99);}while(0);
+#define TODO(msg) do{printf("%s.%d: " msg "\n", __FILE__, __LINE__); exit(99);}while(0);
 
-#define LOG(fmt, ...) do{printf(fmt, __VA_ARGS__);}while(0);
+#define LOG(fmt, ...) do{printf(fmt, __VA_ARGS__"\n");}while(0);
