5a2d03c30a722001384e4bccb4a1466901a5cff3
Alberto Damo <alberto.damo@gmail.com>
2026-04-27 09:55:29 +0200

    refactor(exec): better loopnz/loopz

--- v8086/exec/exec.cpp
+++ v8086/exec/exec.cpp
@@ -387,8 +387,7 @@ static s32 _exec_conditional_jump(Instruction* instr, CPU* cpu, const u8 predica
   return res;
 }
 
-static s32 _exec_loopnz(Instruction* instr, CPU* cpu, SegmentView segmens[__Num_Segment],
-    u16* old_val, u16* new_val)
+static s32 _exec_loopnz_z(Instruction* instr, CPU* cpu, u8 zf_nzf)
 {
   s32 res=0;
   s16 inc=0;
@@ -408,49 +407,13 @@ static s32 _exec_loopnz(Instruction* instr, CPU* cpu, SegmentView segmens[__Num_
       break;
   }
 
-  *old_val = *cx_reg;
   (*cx_reg)--;
-  *new_val = *cx_reg;
   
   num.t = ConvType::S16;
   num._u16 = *cx_reg;
   _check_set_zero_flag(cpu, num);
 
-  cpu->ip += !flag_get(cpu->flags, ZF) * inc;
-
-  return res;
-}
-
-static s32 _exec_loopz(Instruction* instr, CPU* cpu, SegmentView segmens[__Num_Segment],
-    u16* old_val, u16* new_val)
-{
-  s32 res=0;
-  s16 inc=0;
-  NumConv num;
-  u16* cx_reg = &cpu->regs[FullRegs::cx]._u16;
-
-  switch (instr->args[0].t)
-  {
-    case ArgImm8:
-      inc = instr->args[0].imm8;
-      break;
-    case ArgImm16:
-      inc = instr->args[0].imm16;
-      break;
-    default:
-      assert(0 && "unreachable _exec_conditional_jump");
-      break;
-  }
-
-  *old_val = *cx_reg;
-  (*cx_reg)--;
-  *new_val = *cx_reg;
-  
-  num.t = ConvType::S16;
-  num._u16 = *cx_reg;
-  _check_set_zero_flag(cpu, num);
-
-  cpu->ip += flag_get(cpu->flags, ZF) * inc;
+  cpu->ip += (zf_nzf == flag_get(cpu->flags, ZF)) * inc;
 
   return res;
 }
@@ -475,8 +438,8 @@ s32 InstructionExec(Instruction* const instr, CPU* cpu, SegmentView segmens[__Nu
     case Opcode::jb: return _exec_conditional_jump(instr, cpu, flag_get(cpu->flags, CF));
     case Opcode::jnp: return _exec_conditional_jump(instr, cpu, !flag_get(cpu->flags, PF));
     case Opcode::jp: return _exec_conditional_jump(instr, cpu, flag_get(cpu->flags, PF));
-    case Opcode::loopnz: return _exec_loopnz(instr, cpu, segmens, old_val, new_val);
-    case Opcode::loopz: return _exec_loopz(instr, cpu, segmens, old_val, new_val);
+    case Opcode::loopnz: return _exec_loopnz_z(instr, cpu, false);
+    case Opcode::loopz: return _exec_loopnz_z(instr, cpu, true);
   }
 
   return res;
